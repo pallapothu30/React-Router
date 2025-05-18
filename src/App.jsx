06dd79216a5dff9519b6578ebb1c40be26f2bd2a
Pallapothu Manikanta <pallapothum784@gmail.com>
2025-05-18 09:32:56 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='text-white bg-black text-center px-50 rounded-100 '>ReactRouter Crash Course</h1>
+  )
+}
+
+export default App
